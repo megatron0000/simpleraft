c6672a4b7b3cb1fbdc65abfd297e73c09474c0d7
Vitor Arruda <vitor_pimenta97@hotmail.com>
2019-11-24 19:13:38 -0300

    Add -v flag to testall.sh

--- testall.sh
+++ testall.sh
@@ -1,5 +1,6 @@
 # Tests in order from low-level to high-level
-go test ./storage -failfast -count 2
-go test ./status -failfast -count 2
-go test ./raftlog -failfast -count 2
-go test ./transport -failfast -count 1 # count=1 to avoid issues with simultaneous servers
+# -v for "verbose": enables print-to-console (log.Printf etc.) inside the tests
+go test -v ./storage -failfast -count 2
+go test -v ./status -failfast -count 2
+go test -v ./raftlog -failfast -count 2
+go test -v ./transport -failfast -count 1 # count=1 to avoid issues with simultaneous servers
